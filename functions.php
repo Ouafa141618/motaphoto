c4f7500b000aef67c000fd4e345e75940cf9c9e7
bounyar ouafa <ouafa.bounyar@gmail.com>
2024-05-23 20:18:43 +0200

    Initial commit with child theme structure

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function motaphoto_child_enqueue_styles() {
+    $parent_style = 'twentytwentythree-style'; // Ceci est le handle du style parent
+
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('motaphoto-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array($parent_style),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'motaphoto_child_enqueue_styles');
+?>
