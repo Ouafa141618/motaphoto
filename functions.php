59ca478eff911c919a5be1886650bbb277b75223
bounyar ouafa <ouafa.bounyar@gmail.com>
2024-05-30 19:44:19 +0200

    rectification header et footer et modale

--- functions.php
+++ functions.php
@@ -8,8 +8,56 @@ function motaphoto_child_enqueue_styles() {
         array($parent_style),
         wp_get_theme()->get('Version')
     );
-}
 
+    // Enqueue Google Fonts
+    wp_enqueue_style('motaphoto-google-fonts', 'https://fonts.googleapis.com/css2?family=Space+Mono&family=Poppins&display=swap', false);
+
+    // Enqueue FontAwesome
+    wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/e6187c85ca.js', array(), null, true);
+}
 add_action('wp_enqueue_scripts', 'motaphoto_child_enqueue_styles');
+
+// Enregistrer le menu principal
+function register_motaphoto_menus() {
+    register_nav_menus(
+        array(
+            'main-menu' => __('Main Menu', 'motaphoto-child'),
+        )
+    );
+}
+add_action('init', 'register_motaphoto_menus');
+
+
+
+
+// Ajoute la prise en charge des images mises en avant
+add_theme_support('post-thumbnails');
+
+// Ajoute automatiquement le titre du site dans l'en-tête du site
+add_theme_support('title-tag');
+
+// Ajout du menu dans le thème WordPress
+function register_my_menus()
+{
+    register_nav_menu('main-menu', __('Menu principal', 'text-domain'));
+    register_nav_menu('footer-menu', __('Menu du pied de page', 'text-domain'));
+}
+add_action('after_setup_theme', 'register_my_menus');
+
+function montheme_mota_widgets_init()
+{
+    register_sidebar(
+        array(
+            'name'          => "Widget footer",
+            'id'            => 'footer-widget',
+            'description'   => 'Widgets pour le pied de page',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+add_action('widgets_init', 'montheme_mota_widgets_init');
 
 ?>
