712f871e2a29637fbd9b113e9fa6e6c4d1306395
bounyar ouafa <ouafa.bounyar@gmail.com>
2024-05-31 13:42:48 +0200

    Description des changements

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
     <header>
         <div class="container-header">
             <a href="<?php echo home_url('/'); ?>" aria-label="Page d'accueil de Nathalie Mota">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Logo.png" alt="Logo <?php echo bloginfo('name'); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/Logo.png" alt="Logo <?php echo bloginfo('name'); ?>">
             </a>
 
 
