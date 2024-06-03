cfaae659ee31175719c2e0a4c21686ae05160169
bounyar ouafa <ouafa.bounyar@gmail.com>
2024-06-03 14:35:15 +0200

    changement STYLE css MODAL

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -2,6 +2,7 @@
 <div id="myModal" class="modal">
     <!-- Modal content -->
     <div class="modal-content">
+        <span class="close">&times;</span> <!-- Bouton de fermeture -->
         <div class="titre-contact"></div>
         <div class="titre-contact"></div>
 
