848dcf3fe9d3f1dde802573778998572d213a83d
bounyar ouafa <ouafa.bounyar@gmail.com>
2024-06-11 19:28:04 +0200

    modale ok ajustement css en cours

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -2,6 +2,7 @@
     <div class="modal-content">
         <span class="close">&times;</span>
         <div class="titre-contact"></div>
+        <div class="titre-contact"></div>
         <div class="modal-body">
             <?php echo do_shortcode('[contact-form-7 id="b35ae4f" title="Modal"]'); ?>
             <input type="hidden" name="photo_ref" value="">
